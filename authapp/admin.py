1c3aa882a3c0e9449f7f14b96fb2e9c32ed483e9
nikpanfer <121313425+nikpanfer@users.noreply.github.com>
2023-02-05 16:38:39 +0300

    Done lesson 6 (#6)

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,1 +1,24 @@
-# Register your models here.
+from django.contrib import admin
+
+from authapp import models as mainapp_models
+
+
+@admin.register(mainapp_models.CustomUser)
+class NewsAdmin(admin.ModelAdmin):
+    search_fields = ["username", "first_name", "last_name", "email"]
+    list_display = [
+        "id",
+        "username",
+        "first_name",
+        "last_name",
+        "age",
+        "email",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "last_login",
+        "date_joined",
+    ]
+    ordering = ["-is_superuser", "-is_staff", "-is_active", "username"]
+    list_per_page = 15
+    list_filter = ["is_superuser", "is_staff", "is_active"]
